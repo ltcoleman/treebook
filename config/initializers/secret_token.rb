840f4b77d52b210dadf40ba6472f84cc3fa25440
Luke Coleman <luke.coleman@abcfinancial.com>
2013-04-16 15:11:46 -0500

    initial add of rails test app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '013109a876f7de9d04d513e2d409f7f509b0129a0b73ed4bd34cdd7e1782c9ccddb4d6ab96788beb246e9f5d0b81e398af69de7198614b020b127f6fc2ce0259'
